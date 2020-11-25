efefe9446e592f7d293365f8a64e3b5bae899502
Aleksey Kresnikov <kresnikov@yandex.ru>
2020-11-25 23:51:40 +0300

    connected the uuid library

--- server/app.js
+++ server/app.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const { request } = require('http');
 const path = require('path');
 const app = express();
+const { v4 } = require('uuid');
 
-const CONTACTS = [{ id: 1, name: 'Alex', value: '234-456-567', marked: false }];
+const CONTACTS = [
+  { id: v4(), name: 'Alex', value: '234-456-567', marked: false },
+];
 
 // GET
 app.get('/api/contacts', (req, res) => {
   res.status(200).json(CONTACTS);
 });
+
+// POST
+app.post('/api/contacts', (req, res) => {});
 
 app.use(express.static(path.resolve(__dirname, '../client')));
 
